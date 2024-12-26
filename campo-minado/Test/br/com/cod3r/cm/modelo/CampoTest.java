eadca6053aa23fa19d10e858100f3853ace8b8bd
Potatoyz908 <euller2005@gmail.com>
2024-12-26 13:25:16 -0300

    Refactor(Campo): Atualiza lógica de adição de vizinhos e abertura de campo

--- campo-minado/Test/br/com/cod3r/cm/modelo/CampoTest.java
+++ campo-minado/Test/br/com/cod3r/cm/modelo/CampoTest.java
@@ -1,5 +1,6 @@
 package br.com.cod3r.cm.modelo;
 
+import br.com.cod3r.cm.excecao.ExplosaoException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -8,44 +9,50 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CampoTest {
 
-    private  Campo campo = new Campo(3, 3);
+    private Campo campo = new Campo(3, 3);
 
     @BeforeEach
-    void iniciarCampo(){
+    void iniciarCampo() {
         campo = new Campo(3, 3);
     }
+
     @Test
-    void testeVizinhoRealDistancia1Esquerda(){
+    void testeVizinhoRealDistancia1Esquerda() {
         Campo vizinho = new Campo(3, 2);
         boolean resultado = campo.adicionarVizinho(vizinho);
         assertTrue(resultado);
     }
+
     @Test
-    void testeVizinhoRealDistancia1Direita(){
+    void testeVizinhoRealDistancia1Direita() {
         Campo vizinho = new Campo(3, 4);
         boolean resultado = campo.adicionarVizinho(vizinho);
         assertTrue(resultado);
     }
+
     @Test
-    void testeVizinhoRealDistancia1EmCima(){
+    void testeVizinhoRealDistancia1EmCima() {
         Campo vizinho = new Campo(2, 3);
         boolean resultado = campo.adicionarVizinho(vizinho);
         assertTrue(resultado);
     }
+
     @Test
-    void testeVizinhoRealDistancia1EmBaixo(){
+    void testeVizinhoRealDistancia1EmBaixo() {
         Campo vizinho = new Campo(4, 3);
         boolean resultado = campo.adicionarVizinho(vizinho);
         assertTrue(resultado);
     }
+
     @Test
-    void testeVizinhoRealDistancia2(){
+    void testeVizinhoRealDistancia2() {
         Campo vizinho = new Campo(2, 2);
         boolean resultado = campo.adicionarVizinho(vizinho);
         assertTrue(resultado);
     }
+
     @Test
-    void testeNaoVizinho(){
+    void testeNaoVizinho() {
         Campo vizinho = new Campo(1, 1);
         boolean resultado = campo.adicionarVizinho(vizinho);
         assertTrue(!resultado);
